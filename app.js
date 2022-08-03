2e756d568bee8eed16b75692d895fc8bd4d3410a
lucasplr <lucasriffel55@gmail.com>
2022-08-02 23:16:06 -0300

    Starting a new project

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+//view engine
+app.set('view engine', 'ejs')
+
+
+//static
+app.use(express.static('public'))
+
+//bodyParser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', (req,res) => {
+    res.render('index')
+    console.log('teste')
+})
+
+
+
+
+
+app.listen(8080, () => {
+    console.log('Servidor rodando')
+})
