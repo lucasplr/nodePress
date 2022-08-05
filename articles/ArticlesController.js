e08989531acf8edff0781df778eca4ef2e3b9b6b
lucasplr <lucasriffel55@gmail.com>
2022-08-05 02:20:51 -0300

    articles create and list page

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,35 @@
 const express = require('express')
+const slugify = require('slugify')
 const router = express.Router()
+const Category = require('../categories/Category')
+const Article = require('./Article')
 
-router.get('/articles', (req,res) => {
-    res.send('ROTA DE ARTIGOS')
+router.get('/admin/articles', (req,res) => {
+    Article.findAll().then(articles => {
+        res.render('admin/articles/index', {articles: articles})
+    })
+    
 })
 
 router.get('/admin/articles/new', (req,res) => {
-    res.render('admin/articles/new')
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', {categories: categories})
+    })
+})
+
+router.post('/articles/save', (req,res) => {
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.category
+    
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect('/admin/articles')
+    })
 })
 
 
--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize')
 const connection = new Sequelize('nodepress', 'root', '@Lucasplr321', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 })
 
 module.exports = connection
