a6de0da71a1731260818d719a2eaf8c155e39633
lucasplr <lucasriffel55@gmail.com>
2022-08-07 21:37:18 -0300

    page

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -81,4 +81,34 @@ router.post('/articles/update', (req,res) => {
     })
 })
 
+router.get('/articles/page/:num', (req,res) => {
+    var page = req.params.num
+    var offset = 0
+
+    if(isNaN(page) || page == 1){
+        offset = 0
+    }else{
+        offset = parseInt(page) * 4
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset
+    }).then(articles => {
+
+        var next
+        if(offset + 4 >= articles.count){
+            next = false
+        }else{
+            next = true
+        }
+
+        var result = {
+            next: next,
+            articles: articles 
+        }
+        res.json(result)
+    })
+})
+
 module.exports = router
